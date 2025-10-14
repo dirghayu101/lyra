18d0018bdbe91c0f358d29736be60a83e3f6ce28
Dirghayu Joshi <dirghayujoshi48@gmail.com>
2025-10-14 15:48:41 -0400

    TRPC setup. Gonna modify current state of file.

--- src/app/(dashboard)/page.tsx
+++ src/app/(dashboard)/page.tsx
@@ -3,12 +3,8 @@ import { headers } from "next/headers";
 import { auth } from "@/lib/auth";
 import { HomeView } from "@/modules/home/ui/views/home-view";
 import { redirect } from "next/navigation";
-import { caller } from "@/trpc/server";
-
 
 const Page = async () => {
-
-  const data = await caller.hello({text: "Joshi Server"})
 
   const session = await auth.api.getSession({
     headers: await headers(),
@@ -17,7 +13,6 @@ const Page = async () => {
   if(!session){
     redirect("/sign-in");
   }
-  return <p>{data.greeting}</p>
   return (
     <HomeView />
 
