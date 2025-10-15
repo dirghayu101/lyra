fd61029377aa9de15cddc24b6ef530215be9168d
Dirghayu Joshi <dirghayujoshi48@gmail.com>
2025-10-15 10:39:25 -0400

    Minor changes for responsiveness.

--- src/modules/agents/ui/views/agents-view.tsx
+++ src/modules/agents/ui/views/agents-view.tsx
@@ -5,6 +5,8 @@ import { useTRPC } from "@/trpc/client";
 
 import { LoadingState } from "@/components/loading-state";
 import { ErrorState } from "@/components/error-state";
+import { ResponsiveDialog } from "@/components/responsive-dialog";
+import { Button } from "@/components/ui/button";
 
 export const AgentsViewLoading = () => {
   return (
@@ -28,5 +30,16 @@ export const AgentsView = () => {
   const trpc = useTRPC();
   const { data } = useSuspenseQuery(trpc.agents.getMany.queryOptions());
 
-  return <div>{JSON.stringify(data, null, 2)}</div>;
+  return <div>
+    <ResponsiveDialog 
+      title="Responsive Test"
+      description="Responsive"
+      open
+      onOpenChange={() => {}}
+    >
+
+      <Button>Some Action</Button>
+    </ResponsiveDialog>
+    {JSON.stringify(data, null, 2)}
+    </div>;
 };
